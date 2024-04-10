c27bd9c092ef14e110eedbeaaefab87a8a2f522a
cai-fuqiang <cai-fuqiang@users.noreply.github.com>
2024-04-10 14:47:06 +0000

    deploy: feea9dd6519fe0e9eaa204e7f6003f8cfc0c724d

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1712760425',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ],allowHosts: [ 'chirpy-img.netlify.app', 'fonts.googleapis.com', 'fonts.gstatic.com', 'fonts.googleapis.com', 'cdn.jsdelivr.net', 'cdnjs.cloudflare.com' ],denyPaths: [ ], purge: false };
