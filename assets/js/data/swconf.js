b9a154611ec4fb3fe1ddcc8a61dcd43d6e3139f7
cai-fuqiang <cai-fuqiang@users.noreply.github.com>
2025-12-25 10:18:31 +0000

    deploy: 73eb52ea8a2e924d7d5bad35fc7429202c06a18e

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1766657755',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1766657910',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
